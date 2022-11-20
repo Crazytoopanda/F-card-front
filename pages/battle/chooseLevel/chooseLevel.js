7c436e5e433d42f135ec893af9a0238b9741a1c3
Crazytoopanda <Crazytoopanda@users.noreply.github.com>
2022-11-20 21:33:17 +0800

    lfa update 2022/11/20 选择关卡设计

--- pages/battle/chooseLevel/chooseLevel.js
+++ pages/battle/chooseLevel/chooseLevel.js
@@ -5,9 +5,39 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        situationDynamic: 1, 
+        touchStartLoca: 0, 
+        touchEndLoca: 0
     },
-
+    /**
+     * 触摸事件，开始, 移动与结束
+     */
+    touchStartOper: function(e) {
+        this.data.touchStartLoca = e.touches[0].pageX
+        this.data.touchEndLoca = e.touches[0].pageX
+    },
+    touchMoveOper: function(e) {
+        this.data.touchEndLoca = e.touches[0].pageX
+    },
+    touchEndOper: function(e) {
+        let start = this.data.touchStartLoca
+        let end = this.data.touchEndLoca
+        if(start < end - 25) {
+            let temp = this.data.situationDynamic
+            if(temp > 1) {
+                this.setData({
+                    situationDynamic: temp - 1
+                })
+            }
+        } else if(start > end + 25) {
+            let temp = this.data.situationDynamic
+            if(temp < 9) {
+                this.setData({
+                    situationDynamic: temp + 1
+                })
+            }
+        }
+    },
     /**
      * 生命周期函数--监听页面加载
      */
