c350c2ba722f5eb0f06e02faa8ce67587e727ace
Eniry <2412039308@qq.com>
2022-11-22 01:24:33 +0800

    商店抽卡调用接口实现

--- pages/ui/store/store.js
+++ pages/ui/store/store.js
@@ -1,4 +1,9 @@
 // pages/ui/store/store.js
+const app = getApp()
+var uid = app.global.uid
+var collegeInfluence = app.global.collegeInfluence
+var coin = app.global.coin
+
 Page({
 
     /**
@@ -8,7 +13,71 @@ Page({
         hid_drawCard:true,
         hid_help:true,
         hid_lightBox1:true,
-        hid_lightBox2:true
+        hid_lightBox2:true,
+        drawedCard_name: "",
+        rank: ""
+    },
+
+    /**
+     * 用户点击“返回”返回到主界面
+     */
+    gotoMain:function(){
+        wx.navigateTo({
+            url: "../../../pages/ui/mainDisplay/mainDisplay",
+        })
+
+    },
+
+    /**
+     * 用户点击“走访”抽卡一次
+     */
+    drawCard:function(){
+        if (coin < 500) {
+            wx.showToast({
+              title: '金币不足！',
+            })
+        }
+        else{
+            app.global.coin = coin - 500
+            wx.request({
+                url:'http://www.fcard.site:8088/personalCard/rollpersonalcard', //必填，其他的都可以不填
+                data:{  
+                   uid, 
+                   collegeInfluence
+                },
+                header:{  
+                   'content-type':'application/json'
+                },
+                method:'POST',  
+                dataType:'JSON',  
+                responseType:'JSON', 
+                success(res){
+                    console.log(res)
+                    this.setData({
+                        drawedCard_name: res.data.cardName,
+                        rank: res.data.rank,
+                        hid_drawCard: !this.data.hid_drawCard,
+                        hid_lightBox1: !this.data.hid_lightBox1
+                    })
+
+                },
+                fail(){  
+                    console.log('fail')
+                }
+            })
+        }
+    
+    },
+
+    /**
+     * 用户点击“？”显示帮助
+     */
+    viewHelp:function(){
+        this.setData({
+            hid_help:!this.data.hid_help,
+            hid_lightBox2:!this.data.hid_lightBox2
+        })
+
     },
 
     /**
@@ -64,37 +133,6 @@ Page({
      * 用户点击右上角分享
      */
     onShareAppMessage() {
-
-    },
-
-    /**
-     * 用户点击“走访”抽卡一次
-     */
-    gotoMain:function(){
-        wx.navigateTo({
-            url: "../../../pages/ui/mainDisplay/mainDisplay",
-        })
-
-    },
-    /**
-     * 用户点击“走访”抽卡一次
-     */
-    drawCard:function(){
-            this.setData({
-                hid_drawCard:!this.data.hid_drawCard,
-                hid_lightBox1:!this.data.hid_lightBox1
-            })
-    
-    },
-
-    /**
-     * 用户点击“？”显示帮助
-     */
-    viewHelp:function(){
-        this.setData({
-            hid_help:!this.data.hid_help,
-            hid_lightBox2:!this.data.hid_lightBox2
-        })
 
     }
 })
